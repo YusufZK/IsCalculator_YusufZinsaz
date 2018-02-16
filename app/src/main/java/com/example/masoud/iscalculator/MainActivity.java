218964811e504bfc97e55e49d0fda3d196b70789
Mr.Robot <yusufzk78@gmail.com>
2018-02-16 22:36:44 +0330

    i add To Method Onclick for TextView (that is Button in xml file ok ) ;

--- app/src/main/java/com/example/masoud/iscalculator/MainActivity.java
+++ app/src/main/java/com/example/masoud/iscalculator/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.masoud.iscalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView1 , txtRes ,  textView2 ;
+
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+  protected void onCreate(Bundle savedInstanceState)
+  {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+
+        textView1 = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
+        txtRes = findViewById(R.id.txt_result);
+
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtRes.setText(textView1.getTag().toString());
+            }
+        });
+
+      textView2.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              txtRes.setText(textView2.getTag().toString());
+          }
+      });
   }
+
+
+
+
+
 }
